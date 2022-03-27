9fb26b733c76e008e1a841e2254fb8d3bedc7587
Jasper <d3rz4nd3r@gmail.com>
2022-03-27 19:17:30 +0200

    Graphen passen sich jetzt der Höhe des Bildes an

--- Reporter/TelegramReporter.py
+++ Reporter/TelegramReporter.py
@@ -64,13 +64,15 @@ def create_image(file):
 
     coords = coords[-min(20, len(coords)):]
     highest_value = max(coords)
+    lowest_value = min(coords)
+    intervall = highest_value - lowest_value
 
     for i in range(len(coords)):
         if i < len(coords) - 1:
             x1 = i * width / len(coords) + 1
-            y1 = height - coords[i] * height / (highest_value + 1000)
-            x2 = (i + 1) * width / len(coords)
-            y2 = height - (coords[i + 1]) * height / (highest_value + 1000)
+            x2 = (i + 1) * width / len(coords) + 1
+            y1 = height - (coords[i] - lowest_value) / intervall * height
+            y2 = height - (coords[i] - lowest_value) / intervall * height
             draw.line((x1, y1, x2, y2), fill=white, width=l_width)
     img.save("screenshot.jpg")
 
