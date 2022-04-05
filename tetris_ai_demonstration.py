0e84521b7f93a62ea419d8f85c0c6ffecd0f7a60
Jasper <d3rz4nd3r@gmail.com>
2022-04-05 19:22:31 +0200

    Kommentar zu der zu verwendenden Generation hinzugefügt

--- tetris_ai_demonstration.py
+++ tetris_ai_demonstration.py
@@ -438,6 +438,7 @@ if __name__ == '__main__':
     pygame.event.set_blocked(pygame.MOUSEMOTION)
     pygame.time.set_timer(pygame.USEREVENT + 1, config_game['delay'])
 
+    # Die zu verwendende Generation
     config, pop = restore_checkpoint("neat-checkpoint-393")
     genome = best_genome(pop)
     net = neat.nn.FeedForwardNetwork.create(genome, config)
